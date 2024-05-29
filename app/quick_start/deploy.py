185f1a930a10fa0d27240c19882b55ec4344925c
qichang.dong <qichang.dong@videt.cn>
2024-05-30 07:26:23 +0800

    u

--- app/quick_start/deploy.py
+++ app/quick_start/deploy.py
@@ -4,7 +4,7 @@ ic.configureOutput(includeContext=True, argToStringFunction=str)
 ic.lineWrapWidth = 120
 
 
-backend_config = TurbomindEngineConfig(tp=2, cache_max_entry_count=0.8, quant_policy=4)
+backend_config = TurbomindEngineConfig(tp=1, cache_max_entry_count=0.8, quant_policy=4)
 pipe = pipeline('internlm/internlm2-chat-7b',
                 backend_config=backend_config)
 response = pipe(['Hi, pls intro yourself', 'Shanghai is'])
--- app/run.sh
+++ app/run.sh
@@ -3,8 +3,8 @@ if [ -z $gpu ]; then
     gpu=0
 fi
 export CUDA_VISIBLE_DEVICES=$gpu
-# deploy a0
-file=app/quick_start/deploy.py
+# deploy a0 infer_offline
+file=app/quick_start/infer_offline.py
 # nohup python $file $file-gpu$gpu.log 2>&1 &
 # 
 python $file 2>&1  </dev/null | tee $file.log
